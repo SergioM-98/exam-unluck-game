088c4aab38e6244348d7d4b55184d71ed611f286
SERGIO MAESTRALE s346253 <s346253@studenti.polito.it>
2025-06-10 18:35:45 +0200

    refactor: remove unused indexes from Game model and update related functions

--- server/GamesModels.mjs
+++ server/GamesModels.mjs
@@ -1,12 +1,11 @@
 import dayjs from 'dayjs';
 
 
-function Game(gameId, userId, date=null, situations = [], indexes = [], won = []) {
+function Game(gameId, userId, date=null, situations = [], won = []) {
   this.gameId = gameId;
   this.userId = userId;
   this.date = date && dayjs(date) 
   this.situations = situations;
-  this.indexes = indexes;
   this.won = won;
   this.totalWon = this.won.filter(w => w !== null || w !== undefined).length;
 }
--- server/dao-games.mjs
+++ server/dao-games.mjs
@@ -13,7 +13,6 @@ export function getGamesByUser(id_user) {
                     row.id_player,
                     row.date,
                     [row.situation1, row.situation2, row.situation3, row.situation4, row.situation5, row.situation6, row.situation7, row.situation8],
-                    [row.index1, row.index2, row.index3, row.index4, row.index5, row.index6, row.index7, row.index8],
                     [row.won1, row.won2, row.won3, row.won4, row.won5, row.won6, row.won7, row.won8],
                     row.total_won)
                 )
@@ -26,13 +25,9 @@ export function getGamesByUser(id_user) {
 export function addGame(game) {
     return new Promise((resolve, reject) => {
         const query = `INSERT INTO games (id_player, date, situation1, situation2, situation3, situation4, situation5, situation6, situation7, situation8,
-            index1, index2, index3, index4, index5, index6, index7, index8,
             won1, won2, won3, won4, won5, total_won)
-            VALUES (?,
-                ?, ?, ?, ?, ?, ?, ?, ?, ?,
-                ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
         db.run(query, [game.userId, game.date, game.situations[0], game.situations[1], game.situations[2], game.situations[3], game.situations[4], game.situations[5], game.situations[6] ?? null, game.situations[7] ?? null,
-            game.indexes[0], game.indexes[1], game.indexes[2], game.indexes[3], game.indexes[4], game.indexes[5], game.indexes[6] ?? null, game.indexes[7] ?? null,
             game.won[0] ?? null, game.won[1] ?? null, game.won[2] ?? null, game.won[3] ?? null, game.won[4] ?? null, game.totalWon], function(err) {
                 if (err) {
                     reject(err);
@@ -43,7 +38,8 @@ export function addGame(game) {
     });
 }
 
-export function getSituations(situationIds = []) {
+export function getSituations(situationIds = [], numsituations) {
+
     return new Promise((resolve, reject) => {
         const query = 'SELECT * FROM situations';
         db.all(query, [], (err, rows) => {
@@ -60,7 +56,10 @@ export function getSituations(situationIds = []) {
                     row.image,
                     row.index
                 ));
-                const randomSituation = situations[Math.floor(Math.random() * situations.length)];
+                // shuffle the situations
+                const shuffled = situations.sort(() => Math.random() - 0.5);
+                // select random situations card
+                const randomSituation = shuffled.slice(0, numsituations);
                 return resolve(randomSituation);
             }
         });
