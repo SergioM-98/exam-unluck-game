f36acecfbf2bdc162d3b942098be2d23c090dcba
SERGIO MAESTRALE s346253 <s346253@studenti.polito.it>
2025-06-11 19:05:20 +0200

    refactor: update game retrieval and addition functions to use new Card and Round models

--- server/dao-games.mjs
+++ server/dao-games.mjs
@@ -1,67 +1,121 @@
 import db from './db.mjs';
-import {Game, Situation} from './GamesModels.mjs';
+import {Game, Card, Round} from './GamesModels.mjs';
 
-export function getGamesByUser(id_user) {
-    return new Promise((resolve, reject) => {
-        const query = 'SELECT * FROM games WHERE id_user = ?';
-        db.all(query, [id_user], (err, rows) => {
-            if (err) {
-                reject(err);
-            } else {
-                const games = rows.map(row => new Game(
+
+export async function getGamesByUserId(userId) {
+    const query = `SELECT *
+                            FROM games
+                            JOIN cards AS card1 ON games.initial_card1 = card1.id_card
+                            JOIN cards AS card2 ON games.initial_card2 = card2.id_card
+                            JOIN cards AS card3 ON games.initial_card3 = card3.id_card
+                            JOIN rounds AS round1 ON games.round1 = round1.id_round
+                            JOIN rounds AS round2 ON games.round2 = round2.id_round
+                            JOIN rounds AS round3 ON games.round3 = round3.id_round
+                            LEFT JOIN rounds AS round4 ON games.round4 = round4.id_round
+                            LEFT JOIN rounds AS round5 ON games.round5 = round5.id_round
+                            WHERE games.id_player = ?`;
+    db.all(query, [userId], (err, rows) => {
+        if (err) {
+            reject(err);
+        }
+        else {
+            const games = rows.map(row => {
+                return new Game(
                     row.id_game,
                     row.id_player,
                     row.date,
-                    [row.situation1, row.situation2, row.situation3, row.situation4, row.situation5, row.situation6, row.situation7, row.situation8],
-                    [row.won1, row.won2, row.won3, row.won4, row.won5, row.won6, row.won7, row.won8],
-                    row.total_won)
-                )
-                resolve(games);
-            }
-        });
-    });
-}
+                    new Card(row['card1.id_card'], row['card1.name'], row['card1.image'], row['card1.index']),
+                    new Card(row['card2.id_card'], row['card2.name'], row['card2.image'], row['card2.index']),
+                    new Card(row['card3.id_card'], row['card3.name'], row['card3.image'], row['card3.index']),
+                    new Round(row['round1.id_round'], row['round1.started_at'], row['round1.card_id'], row['round_number'], row['round1.won']),
+                    new Round(row['round2.id_round'], row['round2.started_at'], row['round2.card_id'], row['round_number'], row['round2.won']),
+                    new Round(row['round3.id_round'], row['round3.started_at'], row['round3.card_id'], row['round_number'], row['round3.won']),
+                    row.round4 ? new Round(row['round4.id_round'], row['round4.started_at'], row['round4.card_id'], row['round_number'], row['round4.won']) : null,
+                    row.round5 ? new Round(row['round5.id_round'], row['round5.started_at'], row['round5.card_id'], row['round_number'], row['round5.won']) : null,
+                    row.totalWon
+                );
 
-export function addGame(game) {
-    return new Promise((resolve, reject) => {
-        const query = `INSERT INTO games (id_player, date, situation1, situation2, situation3, situation4, situation5, situation6, situation7, situation8,
-            won1, won2, won3, won4, won5, total_won)
-            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
-        db.run(query, [game.userId, game.date, game.situations[0], game.situations[1], game.situations[2], game.situations[3], game.situations[4], game.situations[5], game.situations[6] ?? null, game.situations[7] ?? null,
-            game.won[0] ?? null, game.won[1] ?? null, game.won[2] ?? null, game.won[3] ?? null, game.won[4] ?? null, game.totalWon], function(err) {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve(this.lastID);
-                }
             });
+            resolve(games.sort((a, b) => dayjs(b.date).isAfter(dayjs(a.date)) ? 1 : -1));
+        }
     });
 }
 
-export function getSituations(situationIds = [], numsituations) {
-
+export async function addGame(game) {
     return new Promise((resolve, reject) => {
-        const query = 'SELECT * FROM situations';
-        db.all(query, [], (err, rows) => {
-            if(err) {
+        const query = `INSERT INTO games (id_player, date, initial_card1, initial_card2, initial_card3, round1, round2, round3, round4, round5, total_won)
+                        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+        db.run(query, [
+            game.userId,
+            game.date,
+            game.initialCard1.cardId,
+            game.initialCard2.cardId,
+            game.initialCard3.cardId,
+            game.round1.roundId,
+            game.round2.roundId,
+            game.round3.roundId,
+            game.round4 ? game.round4.roundId : null,
+            game.round5 ? game.round5.roundId : null,
+            game.totalWon
+        ], function(err) {
+            if (err) {
                 reject(err);
-            }
-            else {
-                if (situationIds.length > 0) {
-                    rows = rows.filter(row => !situationIds.includes(row.id_situation));
-                }
-                const situations = rows.map(row => new Situation(
-                    row.id_situation,
-                    row.name,
-                    row.image,
-                    row.index
-                ));
-                // shuffle the situations
-                const shuffled = situations.sort(() => Math.random() - 0.5);
-                // select random situations card
-                const randomSituation = shuffled.slice(0, numsituations);
-                return resolve(randomSituation);
+            } else {
+                resolve(this.lastID);
             }
         });
     });
 }
+
+export async function addRound(round) {
+    return new Promise((resolve, reject) => {
+        const query = `INSERT INTO rounds (started_at, id_card, round_number, won)
+                        VALUES (?, ?, ?, ?)`;
+        db.run(query, [
+            round.startedAt,
+            round.cardId,
+            round.roundNumber,
+            round.won
+        ], function(err) {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(this.lastID);
+            }
+        });
+    });
+}
+// This function retrieves a specified number of cards, excluding those that are already used in the game.
+export async function getCards(bannedCardsId = [], numCards) {
+    return new Promise((resolve, reject) => {
+        const query = `SELECT * FROM cards`;
+        db.all(query, [], (err, rows) => {
+            if (err) {
+                reject(err);
+            } else {
+                // Filter out banned cards and shuffle the remaining cards
+                const cards = rows
+                    .map(row => new Card(row.id_card, row.name, row.image, row.index))
+                    .filter(card => !bannedCardsId.includes(card.cardId));
+                resolve(cards.sort(() => Math.random() - 0.5).slice(0, numCards));
+            }
+        });
+    });
+}
+
+export async function getCardById(cardId) {
+    return new Promise((resolve, reject) => {
+        const query = `SELECT * FROM cards WHERE id_card = ?`;
+        db.get(query, [cardId], (err, row) => {
+            if (err) {
+                reject(err);
+            } else if (!row) {
+                resolve(null);
+            } else {
+                resolve(new Card(row.id_card, row.name, row.image, row.index));
+            }
+        });
+    });
+}
+
+
