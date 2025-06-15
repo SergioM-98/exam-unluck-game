e40c5cecb551d723bb09156f8f03ea5e174c3d0f
SERGIO MAESTRALE s346253 <s346253@studenti.polito.it>
2025-06-15 18:09:02 +0200

    fix: update getUser function to use email instead of username for user retrieval

--- server/dao-users.mjs
+++ server/dao-users.mjs
@@ -1,19 +1,18 @@
 import db from './db.mjs';
 import crypto from 'crypto';
 
-export const getUser = (username, password) => {
+export const getUser = (email, password) => {
   return new Promise((resolve, reject) => {
-    const sql = 'SELECT * FROM users WHERE name = ?';
-    db.get(sql, [username], (err, row) => {
+    const sql = 'SELECT * FROM users WHERE email = ?';
+    db.get(sql, [email], (err, row) => {
       if (err) { 
         reject(err); 
       }
       else if (row === undefined) { 
-        console.log(`User ${username} not found`);
         resolve(false); 
       }
       else {
-        const user = {id: row.id_user, username: row.name, email: row.email};
+        const user = {id: row.id_user, name: row.name, email: row.email};
         
         crypto.scrypt(password, row.salt, 32, function(err, hashedPassword) {
           if (err) reject(err);
