4ea60a8cce9048ab11569d79f6b63be6c9bb345b
SERGIO MAESTRALE s346253 <s346253@studenti.polito.it>
2025-06-15 18:08:38 +0200

    feat: add LoginForm and LogoutButton components for user authentication

---
+++ client/src/components/AuthComponents.jsx
@@ -0,0 +1,58 @@
+import { useActionState } from "react";
+import { Form, Button, Row, Col, Alert, Nav } from 'react-bootstrap';
+import { Link } from 'react-router';
+
+function LoginForm(props) {
+    const [state, formAction, isPending] = useActionState(loginFunction, {username: '', password: ''});
+
+    async function loginFunction(prevState, formData) {
+        const credentials = {
+            username: formData.get('username'),
+            password: formData.get('password'),
+        };
+
+        try {
+            await props.handleLogin(credentials);
+            return { success: true };
+        } catch (error) {
+            return { error: 'Login failed. Check your credentials.' };
+        }
+    }
+
+    return (
+        <>
+            { isPending && <Alert variant="warning">Please, wait for the server's response...</Alert> }
+            <Row>
+                <Col md={6}>
+                    <Form action={formAction}>
+                        <Form.Group controlId='username' className='mb-3'>
+                            <Form.Label>username</Form.Label>
+                            <Form.Control name='username' required />
+                        </Form.Group>
+
+                        <Form.Group controlId='password' className='mb-3'>
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type='password' name='password' required minLength={6} />
+                        </Form.Group>
+
+                        {state.error && <p className="text-danger">{state.error}</p>}
+
+                        <Button variant="dark" type='submit' disabled={isPending}>Login</Button>
+                        <Link className='btn btn-danger mx-2 my-2' to={'/'} disabled={isPending}>Cancel</Link>
+                    </Form>
+                </Col>
+            </Row>
+        </>
+    );
+}
+
+function LogoutButton(props) {
+  return    (<Nav.Link as="button"
+      onClick={props.logout} type="button">
+    
+      Logout
+    </Nav.Link>
+  );
+}
+
+export { LoginForm, LogoutButton };
