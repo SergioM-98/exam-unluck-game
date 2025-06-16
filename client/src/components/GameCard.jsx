5348e26e0966578469cc8c3c7b4803c9c9845531
SERGIO MAESTRALE s346253 <s346253@studenti.polito.it>
2025-06-16 20:51:56 +0200

    feat: add GameCard component for displaying game information

---
+++ client/src/components/GameCard.jsx
@@ -0,0 +1,15 @@
+import { Card } from 'react-bootstrap';
+
+function GameCard(props) {
+  return (
+    <Card className="text-center h-100" style={{ width: "240px", maxWidth: "240px", minWidth: "120px", height: "310px", maxHeight: "310px", margin: "0 6px", display: "flex", flexDirection: "column", justifyContent: "space-between" }}>
+      <Card.Body className="d-flex flex-column justify-content-between h-100 p-2">
+        <Card.Title style={{ fontSize: "1rem", whiteSpace: "normal", wordBreak: "break-word"}} title={props.name}>{props.name}</Card.Title>
+        <Card.Img variant="top" src={props.image} style={{ maxHeight: "100px", objectFit: "contain", width: "100%", margin: "0 auto" }} />
+        <Card.Text className="mt-2" style={{ fontSize: "0.95rem", whiteSpace: "nowrap", overflow: "hidden", textOverflow: "ellipsis" }}>{props.index}</Card.Text>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default GameCard;
