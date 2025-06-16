0ec42d80e51e0b3be410b978190338217583395d
SERGIO MAESTRALE s346253 <s346253@studenti.polito.it>
2025-06-16 20:51:51 +0200

    fix: refactor App component to include game logic and improve routing structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,16 @@
 import { useEffect, useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import './App.css'
+import './App.css';
 import { Routes, Route, Navigate, Outlet } from "react-router";
 import { LoginForm } from "./components/AuthComponents";
 import API from "./API/API.mjs";
 import NavHeader from "./components/NavHeader";
 import HomePage from "./components/HomePage";
 import GameManager from "./components/GameManager";
-import RoundManager from "./components/RoundManager";
 import Profile from "./components/Profile";
 import { Container, Row, Col, Alert } from "react-bootstrap";
-
+import { Game, Round } from "./models/GameModels.mjs";
+import dayjs from "dayjs";
 
 function App() {
   const [loggedIn, setLoggedIn] = useState(false);
@@ -18,11 +18,12 @@ function App() {
   const [user, setUser] = useState("");
   const [state, setState] = useState(1); 
   const [hideLinks, setHideLinks] = useState(false);
+  const [gameData, setGameData] = useState(null);
 
 
    useEffect(() => {
     const checkAuth = async () => {
-      const user = await API.getUserInfo(); // we have the user info here
+      const user = await API.getUserInfo();
       setLoggedIn(true);
       setUser(user);
     };
@@ -33,57 +34,96 @@ function App() {
     try {
       const user = await API.logIn(credentials);
       setLoggedIn(true);
-      setMessage({msg: `Welcome, ${user.name}!`, type: 'success'});
+      setMessage({ msg: `Welcome, ${user.name}!`, type: 'success' });
       setUser(user);
-    }catch(err) {
-      setMessage({msg: err, type: 'danger'});
+    } catch (err) {
+      setMessage({ msg: err, type: 'danger' });
     }
   };
 
   const handleLogout = async () => {
     await API.logOut();
     setLoggedIn(false);
-    // clean up everything
     setMessage('');
   };
 
-  
+  const startGame = async (user) => {
+    const startingFilters = { num: 3, visibility: true };
+    const drawn = await API.getCards(0, startingFilters);
+    drawn.sort((a, b) => a.index - b.index);
 
+    // Pesca la carta del primo round
+    const banned = drawn.map(card => card.cardId);
+    const firstRoundCard = await API.getCards(1, { num: 1, bannedCards: banned, visibility: false });
+
+    // Prepara il round corrente
+    const startRound = dayjs().format("HH:mm:ss");
+    const currentRound = new Round(
+      null,
+      startRound,
+      firstRoundCard[0].cardId,
+      1,
+      false,
+      null
+    );
+
+    setGameData({
+      game: new Game(
+        user._id,
+        dayjs().format('YYYY-MM-DD HH:mm:ss'),
+        drawn[0],
+        drawn[1],
+        drawn[2],
+        null, null, null, null, null, null, 0
+      ),
+      cardsInHand: drawn,
+      cardsDrawn: [...drawn, ...firstRoundCard],
+      cardOfRound: firstRoundCard[0],
+      currentRound,
+      rounds: [currentRound]
+    });
+  };
 
   return (
     <Routes>
-    <Route element={<>
-      <NavHeader
-        loggedIn={loggedIn}
-        handleLogout={handleLogout}
-        user={user}
-        // Nascondi i link solo se sei in gioco
-        hideLinks={hideLinks}
-      />
-      <Container fluid>
-      {message && <Row>
-          <Alert variant={message.type} onClose={() => setMessage('')} dismissible>{message.msg}</Alert>
-        </Row> }
-        <Row>
-          <Col>
-            <Outlet />
-          </Col>
-        </Row>
-      </Container>
-      
-    </>}>
-    <Route path="/" element={ <HomePage loggedIn={loggedIn} user={user} state={state} setState={setState} setHideLinks={setHideLinks} />} />
-    <Route path="/profile/:idProfile" element={<Profile user={user} loggedIn={loggedIn} />} />
-    <Route path='/login' element={loggedIn ? <Navigate replace to='/' /> : <LoginForm handleLogin={handleLogin} />} />
-    <Route path="/games" element={<GameManager LoggedIn = {loggedIn} user={user}/>} >
-      <Route path="rounds/:roundNumber" element={<RoundManager loggedIn={loggedIn}/>} />
-      
-    </Route>
-      <Route path="*" element={<Navigate replace to="/" />} />
-      
-    </Route>
+      <Route element={
+        <>
+          <NavHeader loggedIn={loggedIn} handleLogout={handleLogout} user={user} hideLinks={hideLinks}
+        />
+        <Container fluid>
+          {message && (
+            <Row> <Alert variant={message.type} onClose={() => setMessage('')} dismissible>
+                {message.msg}
+              </Alert>
+            </Row>
+          )}
+            <Row>
+              <Col>
+                <Outlet />
+              </Col>
+            </Row>
+          </Container>
+          </>
+        }
+      >
+        <Route path="/" element={
+          <HomePage loggedIn={loggedIn} user={user} state={state} setState={setState} setHideLinks={setHideLinks} onStartGame={() => startGame(user)}
+          />}
+        />
+        <Route path="/games" element={ <GameManager user={user} gameData={gameData} setMessage={setMessage} startingRounds = {true}/> } />
+        <Route path="/profile/:idProfile" element={<Profile user={user} loggedIn={loggedIn} />}/>
+        <Route path="/login" element={
+            loggedIn ? (
+              <Navigate replace to="/" />
+            ) : (
+              <LoginForm handleLogin={handleLogin} />
+            )
+          }
+        />
+        <Route path="*" element={<Navigate replace to="/" />} />
+      </Route>
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
