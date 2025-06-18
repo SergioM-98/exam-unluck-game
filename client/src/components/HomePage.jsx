aed3f50266af929d92b4920c090d18ab39a71992
SERGIO MAESTRALE s346253 <s346253@studenti.polito.it>
2025-06-18 12:58:39 +0200

    feat: update HomePage text to English for consistency and improved user experience

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -20,16 +20,16 @@ function HomePage (props) {
     <Container className="screen text-center mt-5">
       <Row>
         <Col>
-          <h1 className="title">Benvenuti nella HomePage Stuff Happens</h1>
+          <h1 className="title">Welcome to the Stuff Happens HomePage</h1>
           <h2 className="subtitle">Romantic Edition</h2>
           <div className="btn-home d-flex flex-column align-items-center">
-            <Link className='btn btn-dark mb-2' to="/howToPlay">GUIDA RAPIDA</Link> 
+            <Link className='btn btn-dark mb-2' to="/howToPlay">QUICK GUIDE</Link> 
             {props.loggedIn
               ? <Button variant="dark" className="mb-2" onClick={handlePlay}>
-                  GIOCA ORA
+                  PLAY NOW
                 </Button>
               : <Button variant="dark" className="mb-2" onClick={handlePlay}>
-                  GIOCA ORA DEMO
+                  PLAY DEMO NOW
                 </Button>
             }
           </div>
