c173c76f07ac2a4a6d13b65e4bf95052fcac97fb
SERGIO MAESTRALE s346253 <s346253@studenti.polito.it>
2025-06-18 15:21:57 +0200

    feat: enhance GameCard component styling with border and shadow for better visual appeal

--- client/src/components/GameCard.jsx
+++ client/src/components/GameCard.jsx
@@ -3,7 +3,22 @@ import { Card } from 'react-bootstrap';
 function GameCard(props) {
   const URL = "http://localhost:3001";
   return (
-    <Card className="text-center h-100" style={{ width: "240px", maxWidth: "240px", minWidth: "120px", height: "310px", maxHeight: "310px", margin: "0 6px", display: "flex", flexDirection: "column", justifyContent: "space-between" }}>
+    <Card
+      className="text-center h-100"
+      style={{
+        width: "240px",
+        maxWidth: "240px",
+        minWidth: "120px",
+        height: "310px",
+        maxHeight: "310px",
+        margin: "0 6px",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        border: "3px solid #b30059",
+        boxShadow: "0 0 8px #b3005933"
+      }}
+    >
       <Card.Body className="d-flex flex-column justify-content-between h-100 p-2">
         <Card.Title style={{ fontSize: "1rem", whiteSpace: "normal", wordBreak: "break-word"}} title={props.name}>{props.name}</Card.Title>
         <Card.Img variant="top" src={URL + `${props.image}`} style={{ maxHeight: "100px", objectFit: "contain", width: "100%", margin: "0 auto" }} />
