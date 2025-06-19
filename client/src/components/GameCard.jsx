12d4f5eaf50361a7b3b47d2c00b96a826390e6e3
SERGIO MAESTRALE s346253 <s346253@studenti.polito.it>
2025-06-19 09:28:40 +0200

    fix: adjust height properties and improve image scaling in GameCard component

--- client/src/components/GameCard.jsx
+++ client/src/components/GameCard.jsx
@@ -10,7 +10,8 @@ function GameCard(props) {
         maxWidth: "240px",
         minWidth: "120px",
         height: "310px",
-        maxHeight: "310px",
+        maxHeight: "290px",
+        minHeight: "290px",
         margin: "0 6px",
         display: "flex",
         flexDirection: "column",
@@ -21,8 +22,8 @@ function GameCard(props) {
     >
       <Card.Body className="d-flex flex-column justify-content-between h-100 p-2">
         <Card.Title style={{ fontSize: "1rem", whiteSpace: "normal", wordBreak: "break-word"}} title={props.name}>{props.name}</Card.Title>
-        <Card.Img variant="top" src={URL + `${props.image}`} style={{ maxHeight: "100px", objectFit: "contain", width: "100%", margin: "0 auto" }} />
-        <Card.Text className="mt-2" style={{ fontSize: "0.95rem", whiteSpace: "nowrap", overflow: "hidden", textOverflow: "ellipsis" }}>{props.index}</Card.Text>
+        <Card.Img variant="top" src={URL + `${props.image}`} style={{ maxHeight: "100px", objectFit: "scale-down", width: "100%", margin: "0 auto" }} />
+        <Card.Text className="mt-2" style={{ fontSize: "0.95rem", whiteSpace: "nowrap", overflow: "visible", textOverflow: "ellipsis" }}>{props.index}</Card.Text>
       </Card.Body>
     </Card>
   );
